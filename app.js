2937957823e420f28642de4124c1c937561476ba
Rinkumoni Kalita <rinkukalita17@gmail.com>
2025-09-11 02:54:29 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
+require('dotenv').config();
+console.log("Mongo URI:", process.env.MONGODB_URI); // for debugging
+
+
+
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const port = process.env.PORT || 3004;
 
-require('dotenv').config();
+//require('dotenv').config();
+
+
+
+////mongo db connection
+
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
+
+//// middleware
+
 
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
